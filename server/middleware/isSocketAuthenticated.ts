f3bb6f195214643df129b9b1aa56a01262fc40a5
Msq-9 <menpadimanoj@gmail.com>
2023-09-12 21:18:07 +0530

    playing around socket.io rooms

--- server/middleware/isSocketAuthenticated.ts
+++ server/middleware/isSocketAuthenticated.ts
@@ -8,6 +8,7 @@ import { ExtendedError } from 'socket.io/dist/namespace';
 import { Socket } from 'socket.io';
 import { CbConfig } from '@db/couchbase/connectCouchbase';
 import { User } from '@db/couchbase/Schemas/User';
+import { RichTextDocument } from '@db/couchbase/Schemas/RichTextDocument';
 
 const couchbaseConfig: CbConfig = config.get('couchbase');
 
@@ -28,15 +29,24 @@ const isSocketAuthenticated = async (
       userDocId,
       socket?.couchbase?.bucket
     )) as User;
-    if (user) {
+    const rtDocId = `${couchbaseConfig.bucket}::richTextDocument::${socket.handshake.query.documentId}`;
+    const rtDocument = (await getByDocId(
+      rtDocId,
+      socket?.couchbase?.bucket
+    )) as RichTextDocument;
+    if (
+      user &&
+      (user.id === rtDocument.createdBy ||
+        rtDocument.sharedTo.indexOf(user.id) >= 0)
+    ) {
       socket.user = user;
       return next();
     } else {
-      return next(new Error('User not found'));
+      return next(new Error('User not authenticated.'));
     }
   } catch (err) {
     if (err instanceof Error) return next(err);
-    return next(new Error('User not found'));
+    return next(new Error('User not found.'));
   }
 };
 
--- server/websockets/documents.ts
+++ server/websockets/documents.ts
@@ -19,23 +19,19 @@ const updateDocument = (socket: Socket) => {
           rtDocumentDocId,
           socket?.couchbase?.bucket
         )) as RichTextDocument;
-        const user = socket?.user as User;
-        if (
-          user &&
-          (user.id === document.createdBy ||
-            document.sharedTo.indexOf(user.id) >= 0)
-        ) {
-          const rtDocumentModel: RichTextDocument = {
-            ...document,
-            documentJSON: richTextDocument.documentJSON,
-            updatedAt: Date.now(),
-            updatedBy: user?.id
-          };
 
-          await socket?.couchbase?.bucket
-            ?.defaultCollection()
-            .upsert(rtDocumentDocId, rtDocumentModel);
-        }
+        const user = socket?.user as User;
+
+        const rtDocumentModel: RichTextDocument = {
+          ...document,
+          documentJSON: richTextDocument.documentJSON,
+          updatedAt: Date.now(),
+          updatedBy: user?.id
+        };
+
+        await socket?.couchbase?.bucket
+          ?.defaultCollection()
+          .upsert(rtDocumentDocId, rtDocumentModel);
       } catch (err) {
         if (err instanceof Error) {
           socket.emit('documentError', { message: err.message });
@@ -49,6 +45,8 @@ const updateDocument = (socket: Socket) => {
 export const documentsWebSocket = (io: Server) => {
   io.on('connection', (socket) => {
     console.log(`Socket ${socket.id} connected for user ${socket?.user?.id}`);
+
+    socket.join(`richTextDocumentRoom:${socket.handshake.query.documentId}`);
 
     updateDocument(socket);
 
